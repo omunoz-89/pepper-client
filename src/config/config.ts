58c81f5526b6d302aa1013ae1976a3197644fe33
omunoz <steven.120189@gmail.com>
2022-06-20 21:29:24 -0700

    Initial commit

---
+++ src/config/config.ts
@@ -0,0 +1,15 @@
+const config = {
+    firebase: {
+  apiKey: "AIzaSyCgkL3pTp8ZVcR0VPrgr08i6OgJdgD-0p0",
+  authDomain: "pepper-88f30.firebaseapp.com",
+  projectId: "pepper-88f30",
+  storageBucket: "pepper-88f30.appspot.com",
+  messagingSenderId: "805930569586",
+  appId: "1:805930569586:web:b43144f2f3a22e47b9d7eb"
+    },
+    server: {
+        url: 'http://localhost:8080'
+    }
+}
+
+export default config;
